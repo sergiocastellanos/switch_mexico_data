212d3f171f0224621df35723f509cb29c9055d4c
choforio84 <sayeg_84@hotmail.com>
2016-07-08 11:35:26 -0500

    Fixed x axis title for a graph

--- Data Analysis/Net demand per hour in low escenary/low projection.py
+++ Data Analysis/Net demand per hour in low escenary/low projection.py
@@ -4,6 +4,7 @@
 # In[1]:
 
 import pandas as pd
+import random
 import matplotlib.pyplot as plt
 import numpy as np
 def nearest_value(array,value):
@@ -119,11 +120,11 @@ else:
         
 
 plt.figure(2,figsize=(10,8),dpi = 200)
-plt.scatter(range(1,25,res),[df.xs([a,m,indx_ale],level=range(3))[k].tolist()[i] for i in range(0,24,res)],s=80,c=[np.random.rand(3)]*24,alpha=.6,label="Aleatorio")
-plt.scatter(range(1,25,res),[df.xs([a,m,indx_mean],level=range(3))[k].tolist()[i] for i in range(0,24,res)],s=80,c=[np.random.rand(3)]*24,alpha=.6,label="Promedio")
-plt.scatter(range(1,25,res),[df.xs([a,m,indx_median],level=range(3))[k].tolist()[i] for i in range(0,24,res)],s=80,c=[np.random.rand(3)]*24,alpha=.6,label="Mediana")
-plt.scatter(range(1,25,res),[df.xs([a,m,dfp.xs([a,m])[k,'DiaPico']],level=range(3))[k].tolist()[i] for i in range(0,24,res)],s=80,c=[np.random.rand(3)]*24,alpha=.6,label="Aleatorio")
-plt.xlabel('Dia')
+plt.scatter(range(1,25,res),[df.xs([a,m,indx_ale],level=range(3))[k].tolist()[i] for i in range(0,24,res)],s=80,c=[np.random.rand(3)]*24,alpha=.6,label="Dia Aleatorio ({0})".format(indx_ale))
+plt.scatter(range(1,25,res),[df.xs([a,m,indx_mean],level=range(3))[k].tolist()[i] for i in range(0,24,res)],s=80,c=[np.random.rand(3)]*24,alpha=.6,label="Dia Promedio ({0})".format(indx_mean))
+plt.scatter(range(1,25,res),[df.xs([a,m,indx_median],level=range(3))[k].tolist()[i] for i in range(0,24,res)],s=80,c=[np.random.rand(3)]*24,alpha=.6,label="Dia Mediano ({0})".format(indx_median))
+plt.scatter(range(1,25,res),[df.xs([a,m,dfp.xs([a,m])[k,'DiaPico']],level=range(3))[k].tolist()[i] for i in range(0,24,res)],s=80,c=[np.random.rand(3)]*24,alpha=.6,label="Dia Pico")
+plt.xlabel('Hora')
 plt.ylabel('Demanda')
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 plt.title('Dias significativos en nodo {0}, {1}/{2}'.format(k,m,a))
