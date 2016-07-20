cabd9c6cf7c0d98f947fd4e8675b3e7ad958ee5b
choforio84 <sayeg_84@hotmail.com>
2016-07-20 13:07:20 -0500

    Corrected ssh adress
    Changed the ssh adress to the correct one that the ssh tunnel uses to enter the SWITCH database

--- Loads/Data Analysis/Write to database test.py
+++ Loads/Data Analysis/Write to database test.py
@@ -6,14 +6,14 @@
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
-engine=create_engine('postgresql://sayeg:edema7-Warbled@switch-db2.erg.berkeley.edu:5433/switch_mexico')
+engine=create_engine('postgresql://sayeg:edema7-Warbled@127.0.0.1:5433/switch_mexico')
 
 
 # In[20]:
 
 df=pd.DataFrame(np.random.randn(6,4))
 print df
-df.to_sql('aldo',engine,schema='mexico',if_exists='replace',chunksize=10)
+df.to_sql('Test1',engine,schema='mexico',if_exists='replace',chunksize=10)
 
 
 # In[ ]:
