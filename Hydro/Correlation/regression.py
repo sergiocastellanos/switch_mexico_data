166d0559e5d00751583e546c05cfb0db82bbe92e
LexieCore <lexie@protonmail.ch>
2016-08-06 19:08:52 -0400

    Ordinary least squares

--- Hydro/Correlation/regression.py
+++ Hydro/Correlation/regression.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 import pandas
+from termcolor import colored
 from pandas import DataFrame, Series
 import statsmodels.formula.api as sm
 from sklearn.linear_model import LinearRegression
@@ -17,6 +18,12 @@ metadata = pandas.read_csv("../Data/Production-Drought-Precipitation/chiapas/chi
 columns = metadata.columns.values
 X = metadata[columns[3]]
 Y = metadata[columns[1]]
-print X,Y
+print colored(columns[3],"white")
+result = sm.OLS( Y, X ).fit()
+print result.summary()
+
+X = metadata[columns[2]]
+Y = metadata[columns[1]]
+print colored(columns[2],"white")
 result = sm.OLS( Y, X ).fit()
 print result.summary()
