83bced4bea9b9dd5df85ddd6294f195c2f042235
Debian Live user <user@localhost.localdomain>
2016-08-13 13:43:52 +0000

    respaldo csv

--- Hydro/Capacity Factor/cf_close_up.py
+++ Hydro/Capacity Factor/cf_close_up.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sys
 import random
+import csv
 
 
 
@@ -12,6 +13,30 @@ estado = "chiapas"
 planta = "malpaso"
 
 data = pd.read_csv("../Data/Production/%s/%s.csv"%(estado,planta),index_col=0)#production info
+
+
+
+header = "name_switch,name_prodesen,load_zone,load_area,percentile_25,percentile_50,percentile_75"
+print header
+def storeresults():
+    with open(r"meaty_data.csv", "wb") as csvfile:
+        spamwriter = csv.writer(csvfile)
+        spamwriter.writerow(header)
+        with open("load_area.csv") as csvfile:
+            for line in csv.reader(csvfile):
+                percentile_25,percentile_50,percentile_75 = close_up()
+                for i,e in enumerate(percentile_25): # chacamovement
+                        row = str(line[0])+","+str(line[1])+","+str(line[2])+","+str(line[3])+","+str(percentile_25[i])+","+str(percentile_50[i])+","+str(percentile_75[i])
+                        spamwriter.writerow(row)
+
+
+
+
+
+
+
+
+
 
 def get_anio(anio):
     dates = data.index.values
@@ -38,6 +63,7 @@ def close_up():
     a1 = percentile_50[random.randint(0, len(percentile_50)-1)]
     a2 = percentile_75[random.randint(0, len(percentile_75)-1)]
     anios = [a0,a1,a2]
+    meaty_data = []
     for an in anios:
         k = get_anio(an)
         c_up = data[k:k+12]
@@ -46,9 +72,14 @@ def close_up():
         for i in range(12):
             a = c_up.loc[ c_up.index.values[i]][0]
             lista.append(a)
-        print capacityFactor(lista,effective_capacity)
+        meaty_data.append(capacityFactor(lista,effective_capacity))
+    return (meaty_data[0],meaty_data[1],meaty_data[2])
 
 
 
 
-close_up()
+
+
+storeresults()
+
+#close_up()
