ceadb501d2cc5eaac51ed5ad74d4441bde32af01
Debian Live user <user@localhost.localdomain>
2016-08-13 19:57:26 +0000

    .

--- Hydro/Capacity Factor/cf_close_up.py
+++ Hydro/Capacity Factor/cf_close_up.py
@@ -2,40 +2,43 @@ import pandas as pd
 import sys
 import random
 import csv
+import os
 
 
 
 percentile_25= [2013,2009,2007]
 percentile_50= [2006,2012,2015]
 percentile_75 = [2010,2011,2008,2014]
+#
+# estado = "chiapas"
+# planta = "malpaso"
+#
+# data = pd.read_csv("../Data/Production/%s/%s.csv"%(estado,planta),index_col=0)#production info
+#
 
-estado = "chiapas"
-planta = "malpaso"
 
-data = pd.read_csv("../Data/Production/%s/%s.csv"%(estado,planta),index_col=0)#production info
-
-
-
-header = "name_switch,name_prodesen,load_zone,load_area,percentile_25,percentile_50,percentile_75"
+header = ["name_switch","name_prodesen","load_zone","load_area","percentile_25","percentile_50","percentile_75"]
 print header
 def storeresults():
     with open(r"meaty_data.csv", "wb") as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=' ',
+        spamwriter = csv.writer(csvfile, delimiter=',',
                              quotechar=' ', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(header)
+        spamwriter.writerow("""""header""""")
+
         with open("load_area.csv") as csvfile:
             for line in csv.reader(csvfile):
-                percentile_25,percentile_50,percentile_75 = close_up()
-                for i,e in enumerate(percentile_25): # chacamovement
-                        row = str(line[0]),",",str(line[1]),",",str(line[2]),",",str(line[3]),",",str(percentile_25[i]),",",str(percentile_50[i]),","+str(percentile_75[i])
-                        print row
-                        spamwriter.writerow([row])
-
-
-
-
-
-
+                if line[0] == "name_switch":pass
+                else:
+                    planta = line[0]
+                    try:
+                        percentile_25,percentile_50,percentile_75 = close_up(planta)
+                        
+                        for i,e in enumerate(percentile_25): # chacamovement
+                                row = line[0],line[1],line[2],line[3],percentile_25[i],percentile_50[i],percentile_75[i]
+                                #print "row"
+                                spamwriter.writerow(row)
+                    except IOError:
+                        pass
 
 
 def get_anio(anio):
@@ -53,8 +56,10 @@ def capacityFactor(data, mw):
     return lista
 
 
-def close_up():
-    data = pd.read_csv("../Data/Production/%s/%s.csv" % (estado, planta), index_col=0)  # production info
+def close_up(planta):
+    estados  = os.listdir("../Correlation/plantas/Production-Drought-Precipitation")
+    for estado in estados:
+        data = pd.read_csv("../Data/Production/%s/%s.csv" % (estado,planta), index_col=0)  # production info
     effective_capacity = pd.read_csv("../Data/capacidad_efectiva.csv", index_col=0)  # production info
     cfrecords = pd.read_csv("capacityFactorAD.csv") # capacity factors info
     e_c = effective_capacity[[effective_capacity.columns.values[0]]]
