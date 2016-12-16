3eb27fb208197c816b4f8a6d2b49d31cf2214391
choforio84 <sayeg_84@hotmail.com>
2016-12-15 17:21:22 -0800

    Corrected errors and bugs

--- Generation/Costs Asignation.py
+++ Generation/Costs Asignation.py
@@ -10,7 +10,7 @@ df2=pd.read_csv("data/TechCosts.csv",header=0,index_col=0)
 def nearest_value(array,value):
     idx = (np.abs(array-value)).argmin()
     return array[idx]
-def lista(x):
+def lis(x):
     if isinstance(x,list): return np.asarray(x)
     else: return np.asarray([x])
 #selecting only the generating plants of interest. See report for more details.
@@ -26,7 +26,7 @@ for i in df1.index.tolist():
     for name in ['fixed_o_m','variable_o_m','overnight_cost']:
         #selecting the costs according to the capacity and technology of the plant
         #try and except due to error with length of dataframe
-        try: a=df3.loc[df3['capacity_mw']==nearest_value(lista(df2.loc[df1.loc[i,'gen_tech'],'capacity_mw'].tolist()),df1.loc[i,'capacity_mw'].astype('float'))]
+        try: a=df3.loc[df3['capacity_mw']==nearest_value(lis(df2.loc[df1.loc[i,'gen_tech'],'capacity_mw'].tolist()),df1.loc[i,'capacity_mw'].astype('float'))]
         except KeyError: a=df3
         df1.loc[i,name]=float(a[name])
 
