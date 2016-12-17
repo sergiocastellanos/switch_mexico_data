6b103314f4a346642f543bcc5042a4f4e0640659
choforio84 <sayeg_84@hotmail.com>
2016-12-16 16:44:29 -0800

    Added yearly frecuency and ploting formats
    Changed the script to ask you for a yearly frequency before plotting the
    daily load in all year and removed markers from plot

--- Loads/Data Analysis/Net load per hour in high escenary/Graphical analysis.py
+++ Loads/Data Analysis/Net load per hour in high escenary/Graphical analysis.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[4]:
 
 import pandas as pd
 import random
@@ -15,14 +15,14 @@ df=pd.read_csv("OrganizedTables/HourlyLoadPerNode.csv",index_col=range(4))
 dfp=pd.read_csv("OrganizedTables/LoadHighlightsPerNode.csv",index_col=range(2),header=range(2))
 
 
-# In[4]:
+# In[ ]:
 
 for index, k in enumerate(df.columns.tolist()):
     
     plt.figure(index,figsize=(10,7),dpi = 200)
     
     data1=[]
-    for a in range(2016,2031):
+    for a in range(2016,2031,3):
         try: f=df.xs(a,level=0)[k]
         except ValueError: print "{0},{1},{2}".format(a,m,d)
         yy=[]
@@ -32,17 +32,18 @@ for index, k in enumerate(df.columns.tolist()):
                 except KeyError: yy.append(0)
                 
         
-        color =[np.random.rand(3)]*372
-        plt.scatter(range(1,373), yy, s=20, c=color, alpha=0.5,label='{0}'.format(a))
+        color =np.random.rand(3)
+        plt.plot( yy, c=color, alpha=0.7,label='{0}'.format(a))
     plt.xticks([0,90,180,270], ['January','May','July','October'], rotation='vertical')
     name='Yearly variation, high escenary, in node {0}'.format(k)
     plt.ylabel('Load (MW)')
     plt.title(name)
+    plt.grid()
     plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     plt.show()
 
 
-# In[25]:
+# In[19]:
 
 print 'Hourly analysis of a load area. Given a load area, year and month, it will show a plot of the hourly demand every day of the month and another plot of the selected days: peak day, median day, average (closest to monthly average) day and a random day'
 
@@ -50,18 +51,22 @@ k=raw_input("Load area: ")
 a=int(raw_input("Year: "))
 m=int(raw_input("Month: "))
 res=int(raw_input("Hourly frequency:  "))
+day=int(raw_input("Daily frecuency: "))
+
 
 #daily load variation plot
+get_ipython().magic(u'matplotlib inline')
 plt.figure(1,figsize=(10,8),dpi = 200)
 
-for d in df.xs([a,m,1],level=[0,1,3])[k].index.tolist():
+for d in range(1,len(df.xs([a,m,1],level=[0,1,3])[k].index.tolist()),day):
     yy=[]
     for h in range(1,25):
         yy.append(df.xs([a,m,d,h])[k])
-    plt.scatter(range(1,25,res),[yy[i] for i in range(0,24,res)],s=40,c=[np.random.rand(3)]*24,alpha=.6,label='{0}'.format(d))
+    plt.plot(range(1,25,res),[yy[i] for i in range(0,24,res)],c=np.random.rand(3),alpha=.6,label='{0}'.format(d),marker='o')
 print dfp.xs([a, m])[k]
 plt.ylabel('Load (MW)')
 plt.xlabel('Hour')
+plt.grid()
 plt.title('Daily load variation, high escenary, in {0} at {1}/{2}'.format(k,m,a))
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 plt.show()
@@ -94,17 +99,28 @@ plt.plot(range(1,25,res),[df.xs([a,m,indx_median],level=range(3))[k].tolist()[i]
 plt.plot(range(1,25,res),[df.xs([a,m,dfp.xs([a,m])[k,'PeakDay']],level=range(3))[k].tolist()[i] for i in range(0,24,res)],c=np.random.rand(3),alpha=.6,label="Peak day",marker='o')
 plt.xlabel('Hour')
 plt.ylabel('Load (MW)')
+plt.grid()
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 plt.title('Highligh days, high escenary, in {0} at {1}/{2}'.format(k,m,a))
 plt.show()
 #monthly peak order plot
 
 df1=df.xs([a,m])[k].copy()
-df1.sort(ascending=False)
+df1=df1.sort_values(ascending=False)
 plt.figure(3,figsize=[10,8],dpi=200)
-plt.title("Ordered hourly load on {0} at {1}/{2}".format(k,a,m))
+plt.title("Load duration curve in {0} at {1}/{2}".format(k,a,m))
 plt.ylabel("Load (MW)")
+plt.grid()
 plt.plot(df1.tolist(),c=np.random.rand(3))
 plt.show()
-del df1
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
 
