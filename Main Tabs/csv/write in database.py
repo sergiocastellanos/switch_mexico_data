2108a076edd5e40f5bfe5a32a2c047cd9daf07b8
choforio84 <sayeg_84@hotmail.com>
2016-12-23 10:32:58 -0800

    Updated readme contents

--- Main Tabs/csv/write in database.py
+++ Main Tabs/csv/write in database.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sqlalchemy import create_engine
 import glob
 #asking for swicht database username and password
@@ -11,4 +12,4 @@ engine=create_engine('postgresql://{0}:{1}@127.0.0.1:5433/switch_mexico'.format(
 for file in [x for x in glob.glob("*.csv") if x not in ["loads_high.csv","loads_low.csv","loads_mid.csv","la_hourly_demand_low.csv","la_hourly_demand_mid.csv","la_hourly_demand_high.csv"]]:
 	print file.split(".")[0]
 	ex=pd.read_csv(file)
-	ex.to_sql(file.split(".")[0] ,engine, schema="sandbox", if_exists ="replace", chunksize=None)
+	ex.to_sql(file.split(".")[0] ,engine, schema=sch, if_exists ="replace", chunksize=None)
