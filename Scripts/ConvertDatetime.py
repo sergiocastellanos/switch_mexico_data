dde0605352d9cdf10c21f588321e22dee2ffc439
mateoatr <mateo.atr@gmail.com>
2017-01-11 10:53:51 -0800

    Update ConvertDatetime and add doxygen config. file.

--- Scripts/ConvertDatetime.py
+++ Scripts/ConvertDatetime.py
@@ -1,25 +1,23 @@
-"""@package docstring
-ConvertDatetime utility.
-"""
 import pytz
 import datetime
 
+##
+# \brief ConvertDatetime utility; useful to convert datetimes between different timezones.
 class ConvertDatetime:
-    """
-    A class to convert datetimes between different zones using pytz and Python datetimes.
-    """
+    ##
+    # A class to convert datetimes between different zones using pytz and Python datetimes.
+    #
     
     def __init__ (self, tz, year, month, day, hour, minute):
-        """
-        Class constructor.
-        @param self Object pointer.
-        @param tz Timezone.
-        @param year Year.
-        @param month Month.
-        @param day Day.
-        @param hour Hour.
-        @param minute Minute.
-        """
+        ##
+        # Class constructor.
+        # \param self Object pointer.
+        # \param tz Timezone.
+        # \param year Year.
+        # \param month Month.
+        # \param day Day.
+        # \param hour Hour.
+        # \param minute Minute.
         self._datetime = datetime.datetime(year, month, day, hour, minute)
         self.tz = tz
         self._tz = pytz.timezone(ConvertDatetime._tz_string(tz))
@@ -30,25 +28,22 @@ class ConvertDatetime:
         self.minute = minute
 
     def __str__ (self):
-        """
-        Print the datetime.
-        @param self Object pointer.
-        """
+        ##
+        # Print the datetime.
+        # \param self Object pointer.
         return str(self._datetime)
 
     def _tz_string (gmt_tz):
-        """
-        Helper function to convert a simple timezone to a pytz one.
-        @param gmt_tz Timezone.
-        """
+        ##
+        # Helper function to convert a simple timezone to a pytz one.
+        # \param gmt_tz Timezone.
         return "Etc/GMT" + str(gmt_tz)
 
     def ConvertTo (self, gmt_tz):
-        """
-        Convert to the desiring timezone (uses Greenwich Time).
-        @param self Object pointer.
-        @param gmt_tz Timezone to convert to.
-        """
+        ##
+        # Convert to the desiring timezone (uses Greenwich Time).
+        # \param self Object pointer.
+        # \param gmt_tz Timezone to convert to.
         self._datetime = pytz.timezone(ConvertDatetime._tz_string(gmt_tz)).localize(
             datetime.datetime(
                 self.year,
@@ -61,10 +56,9 @@ class ConvertDatetime:
         ConvertDatetime._update(self)
 
     def _update (self):
-        """
-        Update object information after changing the timezone.
-        @param self Object pointer.
-        """
+        ##
+        # Update object information after changing the timezone.
+        # \param self Object pointer.
         self.year = self._datetime.year
         self.month = self._datetime.month
         self.day = self._datetime.day
