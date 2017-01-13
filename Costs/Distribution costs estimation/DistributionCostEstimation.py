fdedb9542c57bbc1699520fc97a4f5b281b37a07
choforio84 <sayeg_84@hotmail.com>
2017-01-13 15:49:47 -0800

    Updated the value to 2015 USD

--- Costs/Distribution costs estimation/DistributionCostEstimation.py
+++ Costs/Distribution costs estimation/DistributionCostEstimation.py
@@ -10,20 +10,19 @@ dfs=pd.read_csv("tables/CategorizedCounties.csv")
 '''The calculations of each of the distribution cost estimates 
 is made using a simplified version of the formulas contained in the report
 for improving performance and for asuring none of our variables becomes cero (as some of them are really small)'''
+annual_cost=1872.5
 for c in dfs.index.tolist():
-    dfs.set_value(c,"DistributionCost2 (millions of MXN)", dfs.loc[c,"Real state with electricity"].astype("int64")*21172.83/(3*dfs["Real state with electricity"].astype('int64').sum())+dfs.loc[c,"land area (km^2)"].astype("int64")*21172.83/(dfs["land area (km^2)"].astype('int64').sum()*3))
+    dfs.set_value(c,"DistributionCost2 (millions of MXN)", dfs.loc[c,"real state with electricity"].astype("int64")*annual_cost/(3*dfs["real state with electricity"].astype('int64').sum())+dfs.loc[c,"land area (km^2)"].astype("int64")*annual_cost/(dfs["land area (km^2)"].astype('int64').sum()*3))
 
-    if str(dfs.loc[c,'County type'])=="urban":
-        dfs.set_value(c,'DistributionCost1 (millions of MXN)',dfs.loc[c,'land area (km^2)'].astype("float64")*.5*.003+dfs.loc[c,"Real state with electricity"].astype("int64")*.005*.003 + dfs.loc[c,"Real state with electricity"].astype("int64")/80 * .035)
+    if str(dfs.loc[c,'county type'])=="urban":
+        dfs.set_value(c,'DistributionCost1 (millions of MXN)',dfs.loc[c,'land area (km^2)'].astype("float64")*.5*.003+dfs.loc[c,"real state with electricity"].astype("int64")*.005*.003 + dfs.loc[c,"real state with electricity"].astype("int64")/80 * .035)
     else:
-        dfs.set_value(c,'DistributionCost1 (millions of MXN)',dfs.loc[c,'land area (km^2)'].astype("float64")*1*.005+dfs.loc[c,"Real state with electricity"].astype("int64")*.05*.005 + dfs.loc[c,"Real state with electricity"].astype("int64")/40 *.025)
+        dfs.set_value(c,'DistributionCost1 (millions of MXN)',dfs.loc[c,'land area (km^2)'].astype("float64")*1*.005+dfs.loc[c,"real state with electricity"].astype("int64")*.05*.005 + dfs.loc[c,"real state with electricity"].astype("int64")/40 *.025)
     dfs.set_value(c,"diference of estimation (millions of MXN)",abs(dfs.loc[c,'DistributionCost1 (millions of MXN)']-dfs.loc[c,"DistributionCost2 (millions of MXN)"]))
 dfs.to_csv("tables/CategorizedCounties.csv",index=False)
 
 
 # In[ ]:
-
-dfs
 
 
 # In[ ]:
