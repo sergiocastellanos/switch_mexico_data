49363c4b14e86f61170cd1ad10ce0d3c32145768
choforio84 <sayeg_84@hotmail.com>
2017-01-13 18:02:25 -0800

    Corrected problems Corrected a problem related to the timeseries weights and the timepoint relation with the peak loads.

--- Loads/Timepoints and timeseries creation.py
+++ Loads/Timepoints and timeseries creation.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[67]:
+# In[1]:
 
 import pandas as pd
 import numpy as np
@@ -11,22 +11,30 @@ def median(lis):
         return lis[((len(lis))/2)-1]
     else: return lis[(len(lis)-1)/2]
 def investment_period(y):
-    if y in range(2016,2021): return 2016
-    elif y in range(2021,2026): return 2021
-    else: return 2026
+    if y in range(2016,2021): return 2020
+    elif y in range(2021,2026): return 2025
+    else: return 2030
 df1=pd.read_csv("Medium scenario/OrganizedTables/HourlyLoadPerNode.csv",index_col=range(4),dtype=np.float64)
+df1['total']=df1.sum(axis=1)
 
 
-# In[68]:
+# In[29]:
+
+l=[]
+for d in df1.loc[(y,m),'total'].index.levels[1]:
+    l.append(df1.loc[(y,m,d),'total'].mean())
+l.index(median(l))
+
+
+# In[32]:
 
 df5=pd.DataFrame()
 for y in range(2016,2031):
+    print y
     for m in range(1,13):
         #peak days timestamp creation
-        df2=df1.xs([y,m])
-        a=df2.max()
-        place=a.idxmax()
-        date=df2[place].idxmax()
+        
+        date=df1.loc[(y,m),'total'].idxmax()
         df3=pd.DataFrame()
         if date[1]%2==0:
             for h in range(1,24,2):
@@ -37,10 +45,10 @@ for y in range(2016,2031):
         df3['timeseries']="{0}{1}P".format(y,str(m).zfill(2))
         #median days timestamp creation
         df4=pd.DataFrame()
-        b=[]
-        for i in range(len(df2.values)):
-            b=b+[x for x in df2.values[i]]
-        d=df2[df2.isin([median(b)])].dropna(how='all').index.tolist()[0][0]
+        l=[]
+        for d in df1.loc[(y,m),'total'].index.levels[1]:
+            l.append(df1.loc[(y,m,d),'total'].mean())
+        d=l.index(median(l))
         for h in range(12):
             df4.loc[h+12,'timestamp']="{0}{1}{2}{3}".format(y,str(m).zfill(2),str(int(d)).zfill(2),str(h*2).zfill(2))
         df4['timeseries']="{0}{1}M".format(y,str(m).zfill(2))
@@ -50,31 +58,39 @@ df5=df5.reset_index(drop=True)
 df5.index.name="timepoint_id"
 
 
-# In[69]:
+# In[35]:
 
 df5.to_csv("../Main Tabs/timepoints.tab",sep="\t")
+df5.to_csv("../Main Tabs/csv/timepoints.csv")
 
 
-# In[70]:
+# In[38]:
 
 df6=pd.DataFrame(index=df5['timeseries'].unique())
 df6.index.name='TIMESERIES'
 for index,row in df6.iterrows():
     df6.loc[index,'ts_period']=investment_period(int(index[:4]))
     if index[-1]=='M': 
-        #calculating scaling factor according to "timescales.py"
-        df6.loc[index,'ts_scale_to_period']= 1*(df1.xs([int(index[:4]),int(index[4:6]),1],level=[0,1,3]).shape[0]-1)*12*5
-    else: df6.loc[index,'ts_scale_to_period']= 1*12*5
+        #calculating scaling factor according to "timescales.py" the scaling factor represents the number
+        #of times that the timeseries is ocurring in a period. See the report for more details.
+        df6.loc[index,'ts_scale_to_period']= (df1.xs([int(index[:4]),int(index[4:6]),1],level=[0,1,3]).shape[0]-1)
+    else: df6.loc[index,'ts_scale_to_period']= 1
 df6['ts_duration_of_tp']=2
-df6['ts_num_tps']=24
+df6['ts_num_tps']=12
 df6['ts_period']=df6['ts_period'].astype('int64')
 
 
-# In[71]:
+# In[ ]:
+
+
+
+
+# In[39]:
 
 #rearreging columns and exporting table
 df6=df6[[0,2,3,1]]
 df6.to_csv("../Main Tabs/timeseries.tab",sep="\t")
+df6.to_csv("../Main Tabs/csv/timeseries.csv")
 
 
 # In[ ]:
