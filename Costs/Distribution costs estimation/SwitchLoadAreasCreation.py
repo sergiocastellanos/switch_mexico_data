050bb7c533853a9f216583d0f1c991cbd13ec358
choforio84 <sayeg_84@hotmail.com>
2017-01-19 17:17:38 -0800

    Changed to code to sum over counties in load zone

--- Costs/Distribution costs estimation/SwitchLoadAreasCreation.py
+++ Costs/Distribution costs estimation/SwitchLoadAreasCreation.py
@@ -1,27 +1,33 @@
 
 # coding: utf-8
 
-# In[26]:
+# In[50]:
 
 import pandas as pd
 import numpy as np
-df1=pd.read_csv('tables/CategorizedCounties.csv',index_col=0,header=0)
+df1=pd.read_csv('tables/CategorizedCounties.csv',header=0)
 df2=pd.read_csv('tables/BalancingAreas.csv',index_col=0,header=0)
-df3=pd.read_csv('../../Main Tabs/csv/lz_peak_loads_high.csv',header=0,index_col=0)
-df2=df2.drop('00-autoabasto_local',axis=0)
+df2=df2.drop('00-autoabasto_local')
+df3=pd.read_csv('../../Loads/High scenario/OrganizedTables/HourlyLoadPerNode.csv',header=0,index_col=range(4))
+df4=pd.read_csv('tables/CountiesLoadZones.csv')
 
 
-# In[27]:
+# In[ ]:
 
-df=pd.DataFrame(index=df2.index,columns=['cost_multipliers','existing_local_td',"local_td_annual_cost_per_mw"])
-df['cost_multipliers']=1
+
+
+
+# In[51]:
+
+df=pd.DataFrame(index=df2.index,columns=['lz_cost_multipliers','existing_local_td',"local_td_annual_cost_per_mw"])
+df['lz_cost_multipliers']=1
 """we calculate the existing local td as the peak demand 
 of that load area multiplied by a factor that compensates 
 ditribution loses. 
 This factor contemplates that 15% of the energy generated is lost 
 during the transmission and distribution."""
 for k in df.index.tolist():
-    df.loc[k,'existing_local_td']=df3.loc[k,"peak_demand_mw"].iloc[0]*100/85
+    df.loc[k,'existing_local_td']=df3[k].max()*100/85
 """Now we must assign a distribution cost to each load zone
 as a first aproach, we will asign the distribution cost of each load area
 to the distribution cost of the county where the load area is located 
@@ -31,53 +37,22 @@ This is not representative as a load area distributes electricity to
 much more counties that the county that gives name to it."""
 
 
-# In[ ]:
+# In[52]:
+
+for index in df1.index:
+    df1.loc[index,'load_zone']=df4.loc[index,'lz']
 
 
+# In[53]:
 
-
-# In[ ]:
-
-
-
-
-# In[28]:
-
-
-for k in  df.index.tolist():
-    try: df.loc[k,'local_td_annual_cost_per_mw']=df1.loc[k[3:],"DistributionCost2 (millions of MXN)"]
-    except: pass
-#manual correction for load zones whose name does not match any county
-df.loc['03-obregon','local_td_annual_cost_per_mw']=df1.loc['cajeme',"DistributionCost2 (millions of MXN)"]
-df.loc['04-los_mochis','local_td_annual_cost_per_mw']=df1.loc['ahome',"DistributionCost2 (millions of MXN)"]
-df.loc['07-juarez','local_td_annual_cost_per_mw']=df1.iloc[235,0]
-df.loc['08-moctezuma','local_td_annual_cost_per_mw']=df1.iloc[1926,0]
-df.loc['11-laguna','local_td_annual_cost_per_mw']=df1.loc['torreon',"DistributionCost2 (millions of MXN)"]
-df.loc['12-rio_escondido','local_td_annual_cost_per_mw']=df1.loc['piedras_negras',"DistributionCost2 (millions of MXN)"]
-df.loc['15-matamoros','local_td_annual_cost_per_mw']=df1.iloc[1999,0]
-df.loc['18-valles','local_td_annual_cost_per_mw']=df1.iloc[1825,0]
-df.loc['19-huasteca','local_td_annual_cost_per_mw']=df1.iloc[2006,0]
-df.loc['29-lazaro_cardenas','local_td_annual_cost_per_mw']=df1.iloc[833,0]
-df.loc['31-central','local_td_annual_cost_per_mw']=df1.loc['coyoacan',"DistributionCost2 (millions of MXN)"]
-df.loc['32-poza_rica','local_td_annual_cost_per_mw']=df1.loc['poza_rica_de_hidalgo',"DistributionCost2 (millions of MXN)"]
-df.loc['35-acapulco','local_td_annual_cost_per_mw']=df1.loc['acapulco_de_juarez',"DistributionCost2 (millions of MXN)"]
-df.loc['36-temascal','local_td_annual_cost_per_mw']=df1.loc['oaxaca_de_juarez',"DistributionCost2 (millions of MXN)"]
-df.loc['39-grijalva','local_td_annual_cost_per_mw']=df1.loc['tuxtla_gutierrez',"DistributionCost2 (millions of MXN)"]
-df.loc['43-cancun','local_td_annual_cost_per_mw']=df1.iloc[1808,0]
-df.loc['44-chetumal','local_td_annual_cost_per_mw']=df1.loc['othon_p._blanco',"DistributionCost2 (millions of MXN)"]
-df.loc['50-villa_constitucion','local_td_annual_cost_per_mw']=df1.loc['comondu',"DistributionCost2 (millions of MXN)"]
-df.loc['51-la_paz','local_td_annual_cost_per_mw']=df1.iloc[18,0]
-
-
-
-
-# In[29]:
-
-for k in  df.index.tolist():
+for k in df.index:
+    if k!='53-mulege': df.loc[k,"local_td_annual_cost_per_mw"]=df1[df1['load_zone']==int(k[0:2])]['DistributionCost2 (millions of MXN)'].sum()
+    df.loc['53-mulege','local_td_annual_cost_per_mw']=2
     df.loc[k,'local_td_annual_cost_per_mw']=df.loc[k,'local_td_annual_cost_per_mw']*(10000000/15.8675574)/df.loc[k,'existing_local_td']
+df
 
 
-# In[31]:
+# In[54]:
 
 df.to_csv('../../Main Tabs/csv/load_zones.csv')
 df.to_csv('../../Main Tabs/load_zones.tab',sep="\t")
