e9ec95344af4d23261d98b36619ba119ebf6d09d
choforio84 <sayeg_84@hotmail.com>
2017-01-25 18:17:29 -0800

    Reviewed tables and corrected problems

--- Generation/variable capacity factors creation.py
+++ Generation/variable capacity factors creation.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[37]:
+# In[1]:
 
 import pandas as pd
 import numpy as np
@@ -14,23 +14,19 @@ time=pd.read_csv('../Main Tabs/timepoints.tab',sep="\t")
 df=pd.DataFrame(index=range(time.shape[0]*len(renewable)),columns=['PROJECT','timepoint','proj_max_capacity_factor'])
 
 
-# In[ ]:
+# In[12]:
 
 renewable=df2['project_name'].tolist()+df3['project_name'].tolist()
-i=0
 for name in renewable:
     print renewable.index(name)
-    df4=pd.read_csv('data/renewable simulations/{0}.csv'.format(name),index_col=range(3))
+    df4=pd.read_csv('data/renewable simulations/{0}.csv'.format(name),index_col=0)
     df.loc[range(renewable.index(name)*time.shape[0],((renewable.index(name)+1)*time.shape[0])),'PROJECT']=name
     for index, row in time.iterrows():
         date=str(row['timestamp'])
-        i=index + renewable.index(name)*time.shape[0]
+        i=index +renewable.index(name)*time.shape[0]
         df.loc[i,'timepoint']=index
-        df.loc[i,'proj_max_capacity_factor']=df4.loc[(int(date[4:6]),int(date[6:8]),int(date[8:10])),'capacity_factor']
-
-
-# In[43]:
-df.to_csv('../Main Tabs/variable_capacity_factors.tab',sep="\t",index=False)
+        df.loc[i,'proj_max_capacity_factor']=df4.loc[int('2014'+date[4:]+'00'),'capacity_factor']
+#df.to_csv('../Main Tabs/variable_capacity_factors.tab',sep="\t",index=False)
 
 
 # In[ ]:
