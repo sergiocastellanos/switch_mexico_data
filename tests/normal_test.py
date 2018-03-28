9827e6af52268f370418a4b5504ac116358a6b5c
pesap <pesapsanchez@gmail.com>
2018-03-28 14:59:09 -0700

    First testing code. Just a simply file check

---
+++ tests/normal_test.py
@@ -0,0 +1,13 @@
+import os
+from context import src
+
+
+script_path = os.path.dirname(__file__)
+parent_path = os.path.dirname(os.path.dirname(__file__))
+data_path = os.path.join(parent_path, 'data/clean/loads')
+output_path  = os.path.join(parent_path, 'data/clean/switch_outputs')
+
+def test_check_folders():
+    assert os.path.exists(os.path.join(parent_path, 'data'))
+    assert os.path.exists(os.path.join(parent_path, 'data/clean'))
+
